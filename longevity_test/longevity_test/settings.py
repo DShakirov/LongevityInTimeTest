850c096e36dd4f975a08238cb52e5c32731fb9fd
Dmitry Shakirov <117525799+DShakirov@users.noreply.github.com>
2023-09-03 17:19:44 +0300

    update settings.py

--- longevity_test/longevity_test/settings.py
+++ longevity_test/longevity_test/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'longevity_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        #'NAME': os.getenv('POSTGRES_DB'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
